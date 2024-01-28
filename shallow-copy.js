32ddd6b356b91e9cdd0bbe5145c1f7671aee061a
Maxime Morlet <mmorlet@astech-solutions.com>
2024-01-28 01:01:10 +0100

    shallow-copy-1

--- shallow-copy.js
+++ shallow-copy.js
@@ -2,6 +2,10 @@
 const ingredientList = ["noodles", {
     list: ["eggs", "flour", "water"],
 }];
+
+// Array.from performs a shallow copy
+// it copies the object and the top-level properties but keep the same properties' values in both object
+// The properties' values that are object reference will point to the same target in both object!
 const ingredientListCopy = Array.from(ingredientList);
 
 console.log(ingredientListCopy);
