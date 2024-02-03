341121a23b497d9b351dbd68ef74209f7b10cb16
Maxime M <MaxiCom.Developpement@gmail.com>
2024-02-03 11:10:25 +0100

    Babel-ES2015-Arrow-Function

---
+++ Babel-ES2015-Arrow-Function.js
@@ -0,0 +1,20 @@
+// These statements do the same thing:
+// [1, 2, 3] Array Literal (shorthand)
+// new Array(1, 2, 3) Array instantiation (classic way)
+
+
+
+
+
+// Babel Input: ES2015 Arrow Function
+[1, 2, 3].map(n => n + 1);
+
+
+
+
+// Same as
+(new Array(1, 2, 3)).map(n => n + 1);
+
+
+// Understand callback
+// Understand arrow func
