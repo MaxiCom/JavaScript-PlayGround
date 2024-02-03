55b75195d0fcb8fa754ab7c1f4f0e004c9b4f83b
Maxime M <MaxiCom.Developpement@gmail.com>
2024-02-03 16:08:29 +0100

    Babel-ES2015-Arrow-Function

--- Babel-ES2015-Arrow-Function.js
+++ Babel-ES2015-Arrow-Function.js
@@ -9,12 +9,26 @@
 // Babel Input: ES2015 Arrow Function
 [1, 2, 3].map(n => n + 1);
 
-
+// Babel Output: ES2009 Equivalent
+[1, 2, 3].map(function (n) {
+    return n + 1;
+});
 
 
 // Same as
 (new Array(1, 2, 3)).map(n => n + 1);
 
+(new Array(1, 2, 3)).map(function (n) {
+    return n + 1;
+})
 
-// Understand callback
+
+
+
+
+
+
+// Understand map()
+// Understand map callback
+// Understand arrow function return
 // Understand arrow func
